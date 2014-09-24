6ee591a25ad2d7ebc86d59c833256f1f92221472
Joao Pedro da Costa Nunes <jo40zinhux@JoaoPedro.local>
2014-09-24 12:33:39 -0300

    botao play size pra todos device

--- JonnyGota/JonnyGota/JAGViewController.m
+++ JonnyGota/JonnyGota/JAGViewController.m
@@ -22,7 +22,7 @@
     SKView *skView = (SKView *)self.view;
     skView.showsFPS = YES;
 //    skView.showsNodeCount = YES;
-    skView.showsPhysics = YES;
+//    skView.showsPhysics = YES;
     
     // Create and configure the scene.
 
